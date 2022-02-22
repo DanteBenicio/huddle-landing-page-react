801b4383e8d206041dfb47673de41b7ff3540210
DanteBenicio <dantecosta79@gmail.com>
2022-02-22 18:13:51 -0300

    main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import './index.css'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+import App from './App';
+import Navbar from './components/Navbar';
+import { GlobalStyle } from './styles/global-styles';
+import { theme } from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <Navbar />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
-  document.getElementById('root')
-)
+  document.getElementById('root'),
+);
