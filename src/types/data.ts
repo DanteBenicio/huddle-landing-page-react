8848cdad1e2154c14b4bffd837b33a0dd3d06032
DanteBenicio <dantecosta79@gmail.com>
2022-02-23 14:40:58 -0300

    create data types

---
+++ src/types/data.ts
@@ -0,0 +1,21 @@
+export type DataProps = {
+  section: {
+    title: string
+    description: string
+    imageSrc: string
+    altImageText: string
+  },
+  section2: {
+    title: string
+    description: string
+    imageSrc: string
+    altImageText: string
+  },
+  section3: {
+    title: string
+    description: string
+    imageSrc: string
+    altImageText: string
+    incrementSpacingBottom: boolean
+  }
+}
