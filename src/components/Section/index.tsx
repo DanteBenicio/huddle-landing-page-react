22b1285354fa823bada9d9240dc53ecd1b4220fe
DanteBenicio <dantecosta79@gmail.com>
2022-02-23 14:45:33 -0300

    removed static content

--- src/components/Section/index.tsx
+++ src/components/Section/index.tsx
@@ -10,14 +10,11 @@ export default function Section({
     <Container incrementSpacingBottom={incrementSpacingBottom}>
       <Wrapper rowReverse={rowReverse}>
         <TextContent>
-          <Title>Grow Together</Title>
-          <Description>
-            Generate meaningful discussions with your audience and build a strong, loyal community.
-            Think of the insightful conversations you miss out on with a feedback form.
-          </Description>
+          <Title>{title}</Title>
+          <Description>{description}</Description>
         </TextContent>
         <ImageWrapper>
-          <Image src="assets/illustration-grow-together.svg" alt="" />
+          <Image src={imageSrc} alt={altImageText} />
         </ImageWrapper>
       </Wrapper>
     </Container>
